91e00c929b668c5699e9925bb0692c9046a2e0c0
iwabi <kjmdkdlel@nate.com>
2018-11-28 02:54:50 +0900

    update

--- ADassignment.py
+++ ADassignment.py
@@ -8,64 +8,120 @@ class SWP(QWidget):
 
     def __init__ (self):
         super().__init__()
+        self.initUI()
 
-        self.Userinput = QTextEdit()
-        self.Userinput.setFixedSize(500,700)
 
-        self.result = QTextEdit()
-        self.result.setFixedSize(500,700)
+    def initUI(self):
 
-        self.button = QToolButton()
-        self.button.setText("CLICK")
-        self.label = QLabel()
-        self.label.setText("코딩 규칙 출처 - \t 국민대 소프트웨어 프로젝트 2")
+        # 1 line UI
+        self.fileLineEdit = QTextEdit()
+        self.fileLineEdit.setReadOnly(True)
+        self.fileLineEdit.setFixedSize(500, 700)
+        self.resultLineEdit = QTextEdit()
+        self.resultLineEdit.setReadOnly(True)
+        self.resultLineEdit.setFixedSize(500, 700)
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.fileLineEdit)
+        hbox.addWidget(self.resultLineEdit)
 
-        mainlayout = QGridLayout()
-        mainlayout.addWidget(self.Userinput,0,0)
-        #mainlayout.addWidget(self.result,0,3)
-        mainlayout.addWidget(self.button,0,1)
-        mainlayout.addWidget(self.label,1,1)
-        self.setLayout(mainlayout)
+        # 2 line UI
+        self.openbutton = QPushButton("File Open")
+        self.clickbutton = QPushButton("CLICK")
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(self.openbutton)
+        hbox1.addWidget(self.clickbutton)
 
-        subLayout = QGridLayout()
-        subLayout.addWidget(self.result)
-        self.pushButton = QPushButton("File Open")
-        self.pushButton.clicked.connect(self.pushButtonClicked)
-        self.label = QLabel()
-        mainlayout.addWidget(self.pushButton,1,0)
-        #mainlayout.addWidget(self.label,0,0)
+        # 3 line UI
+        self.taglabel = QLabel("코드 규칙 출처 - \t  국민대 소프트웨어 프로젝트 2")
+        hbox2 = QHBoxLayout()
+        hbox2.addWidget(self.taglabel)
 
-    def pushButtonClicked(self):
-        faddress = QFileDialog.getOpenFileName(self)
-        fname = faddress[0].split('/')[-1]
-        f = open(fname, 'r')
-        data = f.readline()
-        idx = data.find(";")
+        # 세로 배치
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        self.setLayout(vbox)
+
+
+        # 이벤트 핸들러
+        self.openbutton.clicked.connect(self.openbuttonClicked)
+        self.clickbutton.clicked.connect(self.clickbuttonClicked)
+
+    def openbuttonClicked(self):
+        try:
+            faddress = QFileDialog.getOpenFileName(self)
+            self.fname = faddress[0].split('/')[-1]
+            f = open(self.fname, 'r')
+            data = f.read()
+            self.fileLineEdit.setText(data)
+            self.taglabel.setText(faddress[0])
+        except:
+            self.taglabel.setText("파일이 없습니다.")
+
+
+
+    def clickbuttonClicked(self):
+        special_Character = ['+', '-', '/', '%', '=', '==', '!=', '*', '+=', '-=', '/=', '%=', '<', '>']
+
+        #괄호, 대괄호 (brackets; [] ), 중괄호 (braces; {} ) 의 안쪽에 불필요한 공백을 두지 않는다. ex) [ ]
+        special_Character2 = ['(', '{','[']
+        string = []
+
+        f = open(self.fname, 'r')
+        for line in f:
+            for i in range(len(line)): #line글자 수 만큼 반복해서 검사하고 고침
+                for idx in range(len(line)):
+                    enter = line.find('\n')
+                    if line[idx] in special_Character:
+                        if line[idx-1] != " " and line[idx+1] != " ":
+                            line = line[:idx] + " " + line[idx] + " " + line[idx+1:]
+                        elif line[idx-1] != " ":
+                            line = line[:idx] + " " + line[idx] + line[idx+1:]
+                        elif line[idx+1] != " ":
+                            line = line[:idx] + line[idx] + " " + line[idx+2:]
+                    elif line[enter-1] == ';':
+                        line = line[:-2] + '\n'
+                    elif line[idx] == ':':
+                        if line[idx-1] == " " and line[idx+1] != " ":
+                            line = line[:idx-1] + line[idx] + " " + line[idx+1:]
+                        elif line[idx-1] == " ":
+                            line = line[:idx-1] + line[idx:]
+                        elif line[idx+1] != " ":
+                            line = line[:idx] + line[idx] + " " + line[idx+1:]
+                    elif line[idx] == ',':
+                        if line[idx-1] == " ":
+                            if line[idx+1] != " ": #a ,b
+                                line = line[:idx - 1] + line[idx] + " " + line[idx + 1:]
+                            else: #a , b
+                                line = line[:idx - 1] + line[idx] + line[idx + 1:]
+                        elif line[idx+1] != " ": #a,b
+                            line = line[:idx] + line[idx] + " " + line[idx+1:]
+                    elif line[idx] in special_Character2:
+                        print(idx)
+                        if line[idx + 1] == " ":
+                            line = line[:idx] + line[idx] + line[idx+2:]
+                            print(line)
+                            break # 안멈추면 인덱스 에러
+            string.append(line)
+        f.close()
+        self.writefile(string)
+        self.showfile()
+
+    #파일 읽기
+    def writefile(self, string):
+        f = open(self.fname, 'w')
+        for i in string:
+            f.write(i)
+        f.close()
+    #파일 resultlineEdit에 보이기
+    def showfile(self):
+        f = open(self.fname, 'r')
+        output = f.read()
+        self.resultLineEdit.setText(output)
         f.close()
 
-        fw = open(fname, 'w')
-        fw.write(data[:idx])
-        fw.close()
 
-        fh = open(fname, 'r')
-        input_str = fh.readline()
-        self.Userinput.setText(input_str)
-        self.label.setText(faddress[0])
-
-
-    def writedb(self):
-
-        fh = open(fname, 'w')
-        for line in fh.readline():
-            fh.write(line)
-            #for i in range(len(line)):
-             #   if line.find(';'):
-              #      line = line[:len(line)]
-               #     print(line[i])
-            #self.Userinput.setText(line)
-
-
-#        for line in f.readline():
 
 
 if __name__ == "__main__":
